8d4df8cf438101e24c003b8608d2c5823a7b984e
HAFDIAHMED <ahafdi62@gmail.com>
2021-10-08 18:11:26 +0100

    profile screens are done

--- app/screens/page/page-screen.tsx
+++ app/screens/page/page-screen.tsx
@@ -1,14 +1,34 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { Button, Header, Screen, Text } from "../../components"
+import { Linking, TextStyle, View, ViewStyle } from "react-native"
+import {  Button, Header, Screen, Text } from "../../components"
 import { useNavigation } from "@react-navigation/native"
+
+
 //import { useStores } from "../../models"
-import { color } from "../../theme"
+import { color, spacing } from "../../theme"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
+  
+}
+const BUTTON : ViewStyle = {
+  paddingHorizontal : spacing[4],
+  paddingVertical : spacing[4],
+  backgroundColor : '#F9AF05',
+  margin : 10,
+  
+
+}
+const BUTTON_TEXT : TextStyle = {
+  fontSize : 25,
+  
+}
+const BUTTONS : ViewStyle = {
+  paddingHorizontal : spacing[5],
+  paddingVertical : spacing[5],
+  flexDirection : 'column'
 }
 
 export const PageScreen = observer(function PageScreen() {
@@ -21,12 +41,31 @@ export const PageScreen = observer(function PageScreen() {
   return (
     <Screen style={ROOT} preset="scroll">
       <Header headerText="profile information" onLeftPress={()=>{navigation.goBack()}}  leftIcon="back"/>
-      <Text preset="header" text="heloooodoo" />
-      
+      <View style={BUTTONS}>
       <Button 
-text="hello"
-onPress={()=>console.log("helooo")}
-/>
+      style={BUTTON}
+        text="GITHUB"
+        textStyle={BUTTON_TEXT}
+        onPress={()=>{Linking.openURL("https://github.com/HAFDIAHMED")}}
+      />
+        <Button 
+      style={BUTTON}
+        text="WEBSITE"
+        textStyle={BUTTON_TEXT}
+        onPress={()=>{Linking.openURL("https://ahmedhafdi.com/")}}
+      />
+      <Button 
+      style={BUTTON}
+        text="LINKEDEN"
+        textStyle={BUTTON_TEXT}
+        onPress={()=>{Linking.openURL("https://www.linkedin.com/in/ahmed-hafdi-200528188/")}}
+      />
+      </View>
+     
+      
+
+      
+      
     </Screen>
   )
 })
