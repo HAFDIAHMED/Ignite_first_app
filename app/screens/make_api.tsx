06b5812aecc13251fa629c90436a15ea205848c8
HAFDIAHMED <ahafdi62@gmail.com>
2021-10-11 17:39:50 +0100

    fetch api

--- app/screens/make_api.tsx
+++ app/screens/make_api.tsx
@@ -7,11 +7,9 @@ export default function MAKE(props) {
   
     const getPerson = async () => {
        try {
-        const response = await fetch('http://localhost:8000');
+        const response = await fetch('http://192.168.0.106:3000/person');
         const json = await response.json();
-       
-        setData(json.person);
-        console.log(json.person)
+        setData(json);
       } catch (error) {
         console.error(error);
       } finally {
@@ -20,21 +18,28 @@ export default function MAKE(props) {
     }
   
     useEffect(() => {
-      getPerson()
-      
+      getPerson();
+      console.log('heloo')
     }, []);
   
+    function list_person(){
+        return (
+            <View style={{ flex: 1, padding: 24 }}>
+            <Text >FETCH DATA from "http://192.168.0.106:3000/person"</Text>
+          {isLoading ? <ActivityIndicator/> : (
+            <FlatList
+              data={data}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                <Text>{item.name}, {item.job}</Text>
+              )}
+            />
+          )}
+        </View>
+        )
+
+    }
     return (
-      <View style={{ flex: 1, padding: 24 }}>
-        {isLoading ? <ActivityIndicator/> : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>{item.name}, {item.age}</Text>
-            )}
-          />
-        )}
-      </View>
+      list_person()
     );
 }
